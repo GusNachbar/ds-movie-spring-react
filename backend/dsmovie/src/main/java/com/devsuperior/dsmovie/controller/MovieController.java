6ba408ac9d441dbf5b9403202608c0d7d9338573
Gustavo Breve Alessio Nachbar - GNC <gustavo.nachbar@iteris.com.br>
2022-03-12 22:42:04 -0300

    Find movies

--- backend/dsmovie/src/main/java/com/devsuperior/dsmovie/controller/MovieController.java
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/controller/MovieController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class MovieController {
     @GetMapping
     public Page<MovieDTO> findMovies(Pageable pageable){
         return service.findMovies(pageable);
+    }
+
+    @GetMapping(value ="/{id}")
+    public MovieDTO findMovieById(@PathVariable Long id){
+        return service.findMovie(id);
     }
 }
